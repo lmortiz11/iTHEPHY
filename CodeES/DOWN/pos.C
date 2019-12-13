535cca8063e828ce28d1b4c1a7bb1284ab36f5bf
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2019-12-13 18:03:20 +0100

    ss

--- CodeES/DOWN/pos.C
+++ CodeES/DOWN/pos.C
@@ -7,24 +7,30 @@
 
 void ntp::Loop()
 { Double_t P1_pos=0;
-  Double_t P1_neg=0;
+ Double_t P1_pos_rec=0;
+ Double_t P1_neg=0;
+Double_t P1_neg_rec=0;
  Double_t P1_rec=0;
  
+ Double_t P2_pos_rec=0;
  Double_t P2_pos=0;
-  Double_t P2_neg=0;
+ Double_t P2_neg=0;
+ Double_t P2_neg_rec=0;
  Double_t P2_rec=0;
  
  Double_t sPi_pos=0;
+ Double_t sPi_pos_rec=0;
   Double_t sPi_neg=0;
+  Double_t sPi_neg_rec=0;
  Double_t sPi_rec=0;
  
- Double_t D0_pos=0;
+/* Double_t D0_pos=0;
   Double_t D0_neg=0;
  Double_t D0_rec=0;
  
- Double_t Dst_pos=0;
+ Double_t Dst_pos_rec=0;
   Double_t Dst_neg=0;
- Double_t Dst_rec=0;
+ Double_t Dst_rec=0;*/
  
 
 
@@ -60,18 +66,26 @@ void ntp::Loop()
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
-if (Dst_Reconstructed==1)
+/*if (Dst_Reconstructed==1)
 {
    Dst_rec++; 
     if (Dst_ID>0)
 {
-   Dst_pos++; 
+   Dst_pos_rec++; 
 }
 if (Dst_ID<0)
 {
    Dst_neg++;
 } 
-}  
+}
+if (Dst_ID>0)
+{
+   Dst_pos_rec++; 
+}
+if (Dst_ID<0)
+{
+   Dst_neg++;
+       
        
 if (D0_Reconstructed==1)
 {
@@ -84,12 +98,22 @@ if (D0_ID<0)
 {
    D0_neg++;
 } 
-}  
+}  */
      
 if (P2_Reconstructed==1)
 {
    P2_rec++; 
     if (P2_ID>0)
+{
+   P2_pos_rec++; 
+}
+if (P2_ID<0)
+{
+   P2_neg_rec++;
+} 
+}  
+//------  
+if (P2_ID>0)
 {
    P2_pos++; 
 }
@@ -97,11 +121,23 @@ if (P2_ID<0)
 {
    P2_neg++;
 } 
-}  
        
-if (P1_Reconstructed==1)
+
+       
+       
+       
+       if (P1_Reconstructed==1)
 {
    P1_rec++; 
+    if (P1_ID>0)
+{
+   P1_pos_rec++; 
+}
+if (P1_ID<0)
+{
+   P1_neg_rec++;
+} 
+}  
     if (P1_ID>0)
 {
    P1_pos++; 
@@ -110,11 +146,20 @@ if (P1_ID<0)
 {
    P1_neg++;
 } 
-}  
        
 if (sPi_Reconstructed==1)
 {
    sPi_rec++; 
+    if (sPi_ID>0)
+{
+   sPi_pos_rec++; 
+}
+if (sPi_ID<0)
+{
+   sPi_neg_rec++;
+} 
+}         
+      
     if (sPi_ID>0)
 {
    sPi_pos++; 
@@ -123,9 +168,6 @@ if (sPi_ID<0)
 {
    sPi_neg++;
 } 
-}         
-      
-
       
       // if (Cut(ientry) < 0) continue;
    }
@@ -134,35 +176,35 @@ if (sPi_ID<0)
  
 
        
-    Double_t eff_P1_pos=P1_pos/c;
-    Double_t eff_P1_neg=P1_neg/c;
+    Double_t eff_P1_pos=P1_pos_rec/P1_pos;
+    Double_t eff_P1_neg=P1_neg_rec/P1_neg;
  
-    Double_t eff_P2_pos=P2_pos/c;
-    Double_t eff_P2_neg=P2_neg/c;
+    Double_t eff_P2_pos=P2_pos_rec/P2_pos;
+    Double_t eff_P2_neg=P2_neg_rec/P2_neg;
  
-    Double_t eff_sPi_pos=sPi_pos/c;
-    Double_t eff_sPi_neg=sPi_neg/c;
+    Double_t eff_sPi_pos=sPi_pos_rec/sPi_pos;
+    Double_t eff_sPi_neg=sPi_neg_rec/sPi_neg;
  
     /*Double_t eff_D0_pos=D0_pos/nentries;
     Double_t eff_D0_neg=D0_neg/nentries;
  
-    Double_t eff_Dst_pos=Dst_pos/nentries;
+    Double_t eff_Dst_pos_rec=Dst_pos_rec/nentries;
     Double_t eff_Dst_neg=Dst_neg/nentries;*/
  
     // print the number of recontructed events for each particle;
-    Double_t P1_pos_err=(1/c)*sqrt(P1_pos*(c-P1_pos)/c);
-    Double_t P1_neg_err=(1/c)*sqrt(P1_neg*(c-P1_neg)/c);
+    Double_t P1_pos_err=(1/P1_pos)*sqrt(P1_pos_rec*(P1_pos-P1_pos_rec)/P1_pos);
+    Double_t P1_neg_err=(1/P1_neg)*sqrt(P1_neg_rec*(P1_neg-P1_neg_rec)/P1_neg);
  
-    Double_t P2_pos_err=(1/c)*sqrt(P2_pos*(c-P2_pos)/c);
-    Double_t P2_neg_err=(1/c)*sqrt(P2_neg*(c-P2_neg)/c);
+    Double_t P2_pos_err=(1/P2_pos)*sqrt(P2_pos_rec*(P2_pos-P2_pos_rec)/P2_pos);
+    Double_t P2_neg_err=(1/P2_neg)*sqrt(P2_neg_rec*(P2_neg-P2_neg_rec)/P2_neg);
  
-    Double_t sPi_pos_err=(1/c)*sqrt(sPi_pos*(c-sPi_pos)/c);
-    Double_t sPi_neg_err=(1/c)*sqrt(sPi_neg*(c-sPi_neg)/c);
+    Double_t sPi_pos_err=(1/sPi_pos)*sqrt(sPi_pos_rec*(sPi_pos-sPi_pos_rec)/sPi_pos);
+    Double_t sPi_neg_err=(1/sPi_neg)*sqrt(sPi_neg_rec*(sPi_neg-sPi_neg_rec)/sPi_neg);
  
     /*Double_t D0_pos_err=(1/nentries)*sqrt(D0_pos*(nentries-D0_pos)/nentries);
     Double_t D0_neg_err=(1/nentries)*sqrt(D0_neg*(nentries-D0_neg)/nentries);
  
-    Double_t Dst_pos_err=(1/nentries)*sqrt(Dst_pos*(nentries-Dst_pos)/nentries);
+    Double_t Dst_pos_rec_err=(1/nentries)*sqrt(Dst_pos_rec*(nentries-Dst_pos_rec)/nentries);
     Double_t Dst_neg_err=(1/nentries)*sqrt(Dst_neg*(nentries-Dst_neg)/nentries);*/
  
     // print the number of recontructed events for each particle;
@@ -171,14 +213,15 @@ if (sPi_ID<0)
     
     cout<<"Pions pos efficiency: "<< eff_P1_pos<<" +- "<< P1_pos_err  <<'\n';
     cout<<"Pions neg efficiency: "<< eff_P1_neg<<" +- "<< P1_neg_err  <<'\n';
-    cout<<"Kaons pos efficiency: "<<eff_P2_pos <<" +- "<< P2_pos_err<<'\n';
+    cout<<"Kaons pos efficiency: "<<eff_P2_pos
+        <<" +- "<< P2_pos_err<<'\n';
     cout<<"Kaons neg efficiency: "<<eff_P2_neg <<" +- "<< P2_neg_err<<'\n';
     cout<<"Soft Pions pos efficiency: "<<eff_sPi_pos<<" +- "<< sPi_pos_err<<'\n';
     cout<<"Soft Pions neg efficiency: "<<eff_sPi_neg<<" +- "<< sPi_neg_err<<'\n';
     
    /* cout<<"D0 pos efficiency: "<<eff_D0_pos <<" +- "<< D0_pos_err<<'\n';
     cout<<"D0 neg efficiency: "<<eff_D0_neg <<" +- "<< D0_neg_err<<'\n';
-    cout<<"D*+ pos efficiency: "<<eff_Dst_pos <<" +- "<< Dst_pos_err<<'\n';
+    cout<<"D*+ pos efficiency: "<<eff_Dst_pos_rec <<" +- "<< Dst_pos_rec_err<<'\n';
     cout<<"D*+ neg efficiency: "<<eff_Dst_neg <<" +- "<< Dst_neg_err<<'\n';*/
         
  
