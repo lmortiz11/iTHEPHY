097cb6a98fcbf2b89344adcdd41c3015a0f6c117
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2019-12-17 21:15:12 +0100

    eff+-,err,histo

--- CodeES/DOWN/pn.C
+++ CodeES/DOWN/pn.C
@@ -36,8 +36,118 @@ Double_t D0_rec=0;
  Double_t Dst_neg_rec=0;
  Double_t Dst_rec=0;
  
+TH1F* sPiphi_rec_pos=new TH1F("h1", "sPi_PHI_reco_pos", 50,-4,4);
+    TH1F* sPiphi_pos=new TH1F("h2", "sPi_PHI_pos", 50,-4,4);
+TH1F* sPiphi_rec_neg=new TH1F("h1", "sPi_PHI_reco_neg", 50,-4,4);
+    TH1F* sPiphi_neg=new TH1F("h2", "sPi_PHI_neg", 50,-4,4);
+    
+TH1F* sPieta_rec_pos=new TH1F("h1", "sPi_eta_reco_pos", 50,0,5);
+    TH1* sPieta_pos=new TH1F("h2", "sPi_eta", 50,0,5);
+TH1F* sPieta_rec_neg=new TH1F("h1", "sPi_eta_reco_neg", 50,0,5);
+    TH1* sPieta_neg=new TH1F("h2", "sPi_eta_neg", 50,0,5);
+    
+TH1F* sPipt_rec_pos=new TH1F("h1", "sPi_PT_reco_pos", 50,0,900);
+    TH1* sPipt_pos=new TH1F("h1", "sPi_PT_reco_pos", 50,0,900);
+TH1F* sPipt_rec_neg=new TH1F("h1", "sPi_PT_reco_neg", 50,0,900);
+    TH1* sPipt_neg=new TH1F("h1", "sPi_PT_reco_neg", 50,0,900);
+    
+TH1F* sPitheta_rec_pos=new TH1F("h1", "sPi_THETA_reco_pos", 50,0,0.2);
+    TH1F* sPitheta_pos=new TH1F("h1", "sPi_THETA_reco_pos", 50,0,0.2);
+TH1F* sPitheta_rec_neg=new TH1F("h1", "sPi_THETA_reco_neg", 50,0,0.2);
+    TH1F* sPitheta_neg=new TH1F("h1", "sPi_THETA_reco_neg", 50,0,0.2);
 
-
+//---------------------------- 
+ 
+ TH1F* P1phi_rec_pos=new TH1F("h1", "P1_PHI_reco_pos", 50,-4,4);
+    TH1F* P1phi_pos=new TH1F("h2", "P1_PHI_pos", 50,-4,4);
+ TH1F* P1phi_rec_neg=new TH1F("h1", "P1_PHI_reco_neg", 50,-4,4);
+    TH1F* P1phi_neg=new TH1F("h2", "P1_PHI_neg", 50,-4,4);
+ 
+    
+TH1F* P1eta_rec_pos=new TH1F("h1", "P1_eta_reco_pos", 50,0,5);
+    TH1* P1eta_pos=new TH1F("h2", "P1_eta_pos", 50,0,5);
+TH1F* P1eta_rec_neg=new TH1F("h1", "P1_eta_reco_neg", 50,0,5);
+    TH1* P1eta_neg=new TH1F("h2", "P1_eta_neg", 50,0,5);
+    
+TH1F* P1pt_rec_pos=new TH1F("h1", "P1_PT_reco_pos", 50,0,900);
+    TH1* P1pt_pos=new TH1F("h1", "sPi_PT_reco_pos", 50,0,900);
+ TH1F* P1pt_rec_neg=new TH1F("h1", "P1_PT_reco_neg", 50,0,900);
+    TH1* P1pt_neg=new TH1F("h1", "sPi_PT_reco_neg", 50,0,900);
+    
+TH1* P1theta_rec_pos=new TH1F("h1", "P1_THETA_reco_pos", 50,0,0.2);
+    TH1F* P1theta_pos=new TH1F("h1", "P1_THETA_reco_pos", 50,0,0.2);
+TH1* P1theta_rec_neg=new TH1F("h1", "P1_THETA_reco_neg", 50,0,0.2);
+    TH1F* P1theta_neg=new TH1F("h1", "P1_THETA_reco_neg", 50,0,0.2);
+//-----------------------------
+ 
+ TH1F* P2phi_rec_pos=new TH1F("h1", "P2_PHI_reco_pos", 50,-4,4);
+    TH1F* P2phi_pos=new TH1F("h2", "P2_PHI_pos", 50,-4,4);
+ TH1F* P2phi_rec_neg=new TH1F("h1", "P2_PHI_reco_neg", 50,-4,4);
+    TH1F* P2phi_neg=new TH1F("h2", "P2_PHI_neg", 50,-4,4);
+    
+TH1F* P2eta_rec_pos=new TH1F("h1", "P2_eta_reco_pos", 50,0,5);
+    TH1* P2eta_pos=new TH1F("h2", "P2_eta_pos", 50,0,5);
+ TH1F* P2eta_rec_neg=new TH1F("h1", "P2_eta_reco_neg", 50,0,5);
+    TH1* P2eta_neg=new TH1F("h2", "P2_eta_neg", 50,0,5);
+    
+ TH1F* P2pt_rec_pos=new TH1F("h1", "P2_PT_reco_pos", 50,0,900);
+    TH1* P2pt_pos=new TH1F("h1", "P2_PT_reco_pos", 50,0,900);
+ TH1F* P2pt_rec_neg=new TH1F("h1", "P2_PT_reco_neg", 50,0,900);
+    TH1* P2pt_neg=new TH1F("h1", "P2_PT_reco_neg", 50,0,900);
+    
+TH1F* P2theta_rec_pos=new TH1F("h1", "P2_THETA_reco_pos", 50,0,0.2);
+    TH1F* P2theta_pos=new TH1F("h1", "P2_THETA_reco_pos", 50,0,0.2);
+ TH1F* P2theta_rec_neg=new TH1F("h1", "P2_THETA_reco_neg", 50,0,0.2);
+    TH1F* P2theta_neg=new TH1F("h1", "P2_THETA_reco_neg", 50,0,0.2);
+ 
+ //---------------------------
+ 
+ TH1F* D0phi_rec_pos=new TH1F("h1", "D0_PHI_reco_pos", 50,-4,4);
+    TH1F* D0phi_pos=new TH1F("h2", "D0_PHI_pos", 50,-4,4);
+ TH1F* D0phi_rec_neg=new TH1F("h1", "D0_PHI_reco_neg", 50,-4,4);
+    TH1F* D0phi_neg=new TH1F("h2", "D0_PHI_neg", 50,-4,4);
+    
+TH1F* D0eta_rec_pos=new TH1F("h1", "DO_eta_reco_pos", 50,0,5);
+    TH1* D0eta_pos=new TH1F("h2", "D0_eta_pos", 50,0,5);
+TH1F* D0eta_rec_neg=new TH1F("h1", "DO_eta_reco_neg", 50,0,5);
+    TH1* D0eta_neg=new TH1F("h2", "D0_eta_neg", 50,0,5);
+    
+TH1F* D0pt_rec_pos=new TH1F("h1", "D0_PT_reco_pos", 50,0,900);
+    TH1* D0pt_pos=new TH1F("h1", "D0_PT_reco_pos", 50,0,900);
+ TH1F* D0pt_rec_neg=new TH1F("h1", "D0_PT_reco_pos_neg", 50,0,900);
+    TH1* D0pt_neg=new TH1F("h1", "D0_PT_reco_neg", 50,0,900);
+    
+    
+ TH1F* D0theta_rec_pos=new TH1F("h1", "D0_THETA_reco_pos", 50,0,0.2);
+    TH1F* D0theta_pos=new TH1F("h1", "D0_THETA_reco_pos", 50,0,0.2);
+ TH1F* D0theta_rec_neg=new TH1F("h1", "D0_THETA_reco_neg", 50,0,0.2);
+    TH1F* D0theta_neg=new TH1F("h1", "D0_THETA_reco_neg", 50,0,0.2);
+ 
+ //----------------------------
+ 
+ TH1F* Dstphi_rec_pos=new TH1F("h1", "Dst_PHI_reco_pos", 50,-4,4);
+    TH1F* Dstphi_pos=new TH1F("h2", "Dst_PHI_pos", 50,-4,4);
+  TH1F* Dstphi_rec_neg=new TH1F("h1", "Dst_PHI_reco_neg", 50,-4,4);
+    TH1F* Dstphi_neg=new TH1F("h2", "Dst_PHI_neg", 50,-4,4);
+    
+TH1F* Dsteta_rec_pos=new TH1F("h1", "Dst_eta_reco_pos", 50,0,5);
+    TH1* Dsteta_pos=new TH1F("h2", "Dst_eta_pos", 50,0,5);
+ TH1F* Dsteta_rec_neg=new TH1F("h1", "Dst_eta_reco_neg", 50,0,5);
+    TH1* Dsteta_neg=new TH1F("h2", "Dst_eta_neg", 50,0,5);
+    
+TH1F* Dstpt_rec_pos=new TH1F("h1", "Dst_PT_reco_pos", 50,0,900);
+    TH1* Dstpt_pos=new TH1F("h1", "Dst_PT_reco_pos", 50,0,900);
+ TH1F* Dstpt_rec_neg=new TH1F("h1", "Dst_PT_reco_neg", 50,0,900);
+    TH1* Dstpt_neg=new TH1F("h1", "Dst_PT_reco_neg", 50,0,900);
+ 
+    
+TH1F* Dsttheta_rec_pos=new TH1F("h1", "Dst_THETA_reco_pos", 50,0,0.2);
+    TH1F* Dsttheta_pos=new TH1F("h1", "Dst_THETA_reco_pos", 50,0,0.2);
+ TH1F* Dsttheta_rec_neg=new TH1F("h1", "Dst_THETA_reco_neg", 50,0,0.2);
+    TH1F* Dsttheta_neg=new TH1F("h1", "Dst_THETA_reco_neg", 50,0,0.2);
+ 
+ 
+ 
 //   In a ROOT session, you can do:
 //      root> .L ntp.C
 //      root> ntp t
@@ -70,29 +180,51 @@ Double_t D0_rec=0;
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
+ 
 
-       
+
 if (P1_Reconstructed==1 && P2_Reconstructed==1 && sPi_Reconstructed==1)
 {
    Dst_rec++; 
     if (Dst_ID>0)
 {
    Dst_pos_rec++; 
+    Dsteta_rec_pos->Fill(Dst_ETA);
+    Dstphi_rec_pos->Fill(Dst_PHI);
+    Dstpt_rec_pos->Fill(Dst_PT);
+    Dsttheta_rec_pos->Fill(Dst_THETA);
 }
 if (Dst_ID<0)
 {
    Dst_neg_rec++;
+    Dsteta_rec_neg->Fill(Dst_ETA);
+    Dstphi_rec_neg->Fill(Dst_PHI);
+    Dstpt_rec_neg->Fill(Dst_PT);
+    Dsttheta_rec_neg->Fill(Dst_THETA);
 } 
 }
 //----
+
+       
 if (Dst_ID>0)
 {
-   Dst_pos++; 
+    Dst_pos++; 
+    Dstphi_pos->Fill(Dst_PHI);
+    Dsteta_pos->Fill(Dst_ETA);
+    Dstpt_pos->Fill(Dst_PT);
+    Dsttheta_pos->Fill(Dst_THETA);
+    
 }
 if (Dst_ID<0)
 {
    Dst_neg++;
+    Dsteta_neg->Fill(Dst_ETA);
+    Dstphi_neg->Fill(Dst_PHI);
+    Dstpt_neg->Fill(Dst_PT);
+    Dsttheta_neg->Fill(Dst_THETA);
+    
 }
+    
        
 if (P1_Reconstructed==1 && P2_Reconstructed==1)
 {
@@ -100,45 +232,83 @@ if (P1_Reconstructed==1 && P2_Reconstructed==1)
     if (D0_ID>0)
 {
    D0_pos_rec++; 
+        
+    D0phi_rec_pos->Fill(D0_PHI);
+    D0eta_rec_pos->Fill(D0_ETA);
+    D0pt_rec_pos->Fill(D0_PT);
+    D0theta_rec_pos->Fill(D0_THETA);
 }
 if (D0_ID<0)
 {
    D0_neg_rec++;
-} 
+
+    D0phi_rec_neg->Fill(D0_PHI);
+    D0eta_rec_neg->Fill(D0_ETA);
+    D0pt_rec_neg->Fill(D0_PT);
+    D0theta_rec_neg->Fill(D0_THETA);
+    
+}    
 }  
 //-----
 if (D0_ID>0)
 {
    D0_pos++; 
+    D0phi_pos->Fill(D0_PHI);
+    D0eta_pos->Fill(D0_ETA);
+    D0pt_pos->Fill(D0_PT);
+    D0theta_pos->Fill(D0_THETA);
+    
+    
+    
 }
 if (D0_ID<0)
 {
    D0_neg++;
+    
+    D0phi_neg->Fill(D0_PHI);
+    D0eta_neg->Fill(D0_ETA);
+    D0pt_neg->Fill(D0_PT);
+    D0theta_neg->Fill(D0_THETA);
 } 
      
-
+      
 if (P2_Reconstructed==1)
 {
    P2_rec++; 
     if (P2_ID>0)
 {
    P2_pos_rec++; 
+    P2theta_rec_pos->Fill(P2_THETA);
+    P2pt_rec_pos->Fill(P2_PT);
+    P2phi_rec_pos->Fill(P2_PHI);       
+    P2eta_rec_pos->Fill(P2_ETA);
 }
 if (P2_ID<0)
 {
    P2_neg_rec++;
+    P2eta_rec_neg->Fill(P2_ETA);
+    P2phi_rec_neg->Fill(P2_PHI);      
+    P2pt_rec_neg->Fill(P2_PT);
+    P2theta_rec_neg->Fill(P2_THETA);
 } 
 }  
 //------  
 if (P2_ID>0)
 {
    P2_pos++; 
+    P2theta_pos->Fill(P2_THETA);
+    P2pt_pos->Fill(P2_PT);
+    P2phi_pos->Fill(P2_PHI);       
+    P2eta_pos->Fill(P2_ETA);
 }
 if (P2_ID<0)
 {
    P2_neg++;
+    P2eta_neg->Fill(P2_ETA);
+    P2phi_neg->Fill(P2_PHI);      
+    P2pt_neg->Fill(P2_PT);
+    P2theta_neg->Fill(P2_THETA);
 } 
-       
 
        
        
@@ -148,24 +318,42 @@ if (P1_Reconstructed==1)
    P1_rec++; 
     if (P1_ID>0)
 {
-   P1_pos_rec++; 
+   P1_pos_rec++;
+    P1theta_rec_pos->Fill(P1_THETA); 
+    P1pt_rec_pos->Fill(P1_PT);  
+    P1phi_rec_pos->Fill(P1_PHI);  
+    P1eta_rec_pos->Fill(P1_ETA);
+       
 }
 if (P1_ID<0)
 {
    P1_neg_rec++;
+    P1eta_rec_neg->Fill(P1_ETA);
+    P1phi_rec_neg->Fill(P1_PHI);      
+    P1pt_rec_neg->Fill(P1_PT);
+    P1theta_rec_neg->Fill(P1_THETA);
 } 
 }  
 //------  
 if (P1_ID>0)
 {
    P1_pos++; 
+    P1theta_pos->Fill(P1_THETA); 
+    P1pt_pos->Fill(P1_PT);  
+    P1phi_pos->Fill(P1_PHI);  
+    P1eta_pos->Fill(P1_ETA);
 }
 if (P1_ID<0)
 {
    P1_neg++;
+    P1eta_neg->Fill(P1_ETA);
+    P1phi_neg->Fill(P1_PHI);      
+    P1pt_neg->Fill(P1_PT);
+    P1theta_neg->Fill(P1_THETA);
 } 
-   
+    
 
+    
        
 if (sPi_Reconstructed==1)
 {
@@ -173,20 +361,39 @@ if (sPi_Reconstructed==1)
     if (sPi_ID>0)
 {
    sPi_pos_rec++; 
+    sPipt_rec_pos->Fill(sPi_PT);
+    sPiphi_rec_pos->Fill(sPi_PHI);
+    sPieta_rec_pos->Fill(sPi_ETA);
+    sPitheta_rec_pos->Fill(sPi_THETA);
 }
 if (sPi_ID<0)
 {
    sPi_neg_rec++;
+    sPieta_rec_neg->Fill(sPi_ETA);
+    sPiphi_rec_neg->Fill(sPi_PHI);
+    sPipt_rec_neg->Fill(sPi_PT);
+    sPitheta_rec_neg->Fill(sPi_THETA);
 } 
+
+
+    
 }         
 //------        
     if (sPi_ID>0)
 {
    sPi_pos++; 
+    sPipt_pos->Fill(sPi_PT);
+    sPiphi_pos->Fill(sPi_PHI);
+    sPieta_pos->Fill(sPi_ETA);
+    sPitheta_pos->Fill(sPi_THETA);
 }
 if (sPi_ID<0)
 {
    sPi_neg++;
+    sPieta_neg->Fill(sPi_ETA);
+    sPiphi_neg->Fill(sPi_PHI);
+    sPipt_neg->Fill(sPi_PT);
+    sPitheta_neg->Fill(sPi_THETA);
 } 
       
       // if (Cut(ientry) < 0) continue;
@@ -245,16 +452,300 @@ if (sPi_ID<0)
     cout<<"D0 neg efficiency: "<<eff_D0_neg <<" +- "<< D0_neg_err<<'\n';
     cout<<"D*+ pos efficiency: "<<eff_Dst_pos<<" +- "<< Dst_pos_err<<'\n';
     cout<<"D*+ neg efficiency: "<<eff_Dst_neg <<" +- "<< Dst_neg_err<<'\n';
-        
+
+ 
+sPiphi_rec_pos->Sumw2();
+sPiphi_pos->Sumw2();
+sPiphi_rec_neg->Sumw2();
+sPiphi_neg->Sumw2();
+    
+sPieta_rec_pos->Sumw2();
+sPieta_pos->Sumw2();
+sPieta_rec_neg->Sumw2();
+sPieta_neg->Sumw2();
+    
+sPipt_rec_pos->Sumw2();
+sPipt_pos->Sumw2();
+sPipt_rec_neg->Sumw2();
+sPipt_neg->Sumw2();
+    
+sPitheta_rec_pos->Sumw2();
+sPitheta_pos->Sumw2();
+sPitheta_rec_neg->Sumw2();
+sPitheta_neg->Sumw2(); 
+//---------------------- 
+Dstphi_rec_pos->Sumw2();
+Dstphi_pos->Sumw2();
+Dstphi_rec_neg->Sumw2();
+Dstphi_neg->Sumw2();
+    
+Dsteta_rec_pos->Sumw2();
+Dsteta_pos->Sumw2();
+Dsteta_rec_neg->Sumw2();
+Dsteta_neg->Sumw2();
+    
+Dstpt_rec_pos->Sumw2();
+Dstpt_pos->Sumw2();
+Dstpt_rec_neg->Sumw2();
+Dstpt_neg->Sumw2();
+    
+Dsttheta_rec_pos->Sumw2();
+Dsttheta_pos->Sumw2();
+Dsttheta_rec_neg->Sumw2();
+Dsttheta_neg->Sumw2(); 
+//---------------------- 
+D0phi_rec_pos->Sumw2();
+D0phi_pos->Sumw2();
+D0phi_rec_neg->Sumw2();
+D0phi_neg->Sumw2();
+    
+D0eta_rec_pos->Sumw2();
+D0eta_pos->Sumw2();
+D0eta_rec_neg->Sumw2();
+D0eta_neg->Sumw2();
+    
+D0pt_rec_pos->Sumw2();
+D0pt_pos->Sumw2();
+D0pt_rec_neg->Sumw2();
+D0pt_neg->Sumw2();
+    
+D0theta_rec_pos->Sumw2();
+D0theta_pos->Sumw2();
+D0theta_rec_neg->Sumw2();
+D0theta_neg->Sumw2(); 
+//----------------------
+P2phi_rec_pos->Sumw2();
+P2phi_pos->Sumw2();
+P2phi_rec_neg->Sumw2();
+P2phi_neg->Sumw2();
+    
+P2eta_rec_pos->Sumw2();
+P2eta_pos->Sumw2();
+P2eta_rec_neg->Sumw2();
+P2eta_neg->Sumw2();
+    
+P2pt_rec_pos->Sumw2();
+P2pt_pos->Sumw2();
+P2pt_rec_neg->Sumw2();
+P2pt_neg->Sumw2();
+    
+P2theta_rec_pos->Sumw2();
+P2theta_pos->Sumw2();
+P2theta_rec_neg->Sumw2();
+P2theta_neg->Sumw2();
+//-------------------
+P1phi_rec_pos->Sumw2();
+P1phi_pos->Sumw2();
+P1phi_rec_neg->Sumw2();
+P1phi_neg->Sumw2();
+    
+P1eta_rec_pos->Sumw2();
+P1eta_pos->Sumw2();
+P1eta_rec_neg->Sumw2();
+P1eta_neg->Sumw2();
+    
+P1pt_rec_pos->Sumw2();
+P1pt_pos->Sumw2();
+P1pt_rec_neg->Sumw2();
+P1pt_neg->Sumw2();
+    
+P1theta_rec_pos->Sumw2();
+P1theta_pos->Sumw2();
+P1theta_rec_neg->Sumw2();
+P1theta_neg->Sumw2();
+ 
+
+TH1F *h3_sPiphi_pos = (TH1F*)sPiphi_rec_pos->Clone("h3_phi_pos"); h3_sPiphi_pos->Divide(sPiphi_rec_pos,sPiphi_pos);
+ TH1F *h3_sPiphi_neg = (TH1F*)sPiphi_rec_neg->Clone("h3_phi_neg"); h3_sPiphi_neg->Divide(sPiphi_rec_neg,sPiphi_neg);
+ 
+ TH1F *h3_sPieta_pos = (TH1F*)sPieta_rec_pos->Clone("h3_sPieta_pos"); h3_sPieta_pos->Divide(sPieta_rec_pos,sPieta_pos);
+ TH1F *h3_sPieta_neg = (TH1F*)sPieta_rec_neg->Clone("h3_sPieta_neg"); h3_sPieta_neg->Divide(sPieta_rec_neg,sPieta_neg);
+
+ TH1F *h3_sPipt_pos = (TH1F*)sPipt_rec_pos->Clone("h3_sPipt_pos"); h3_sPipt_pos->Divide(sPipt_rec_pos,sPipt_pos);
+  TH1F *h3_sPipt_neg = (TH1F*)sPipt_rec_neg->Clone("h3_sPipt_neg"); h3_sPipt_neg->Divide(sPipt_rec_neg,sPipt_neg);
+
+ TH1F *h3_sPitheta_pos = (TH1F*)sPitheta_rec_pos->Clone("h3_sPitheta_pos"); h3_sPitheta_pos->Divide(sPitheta_rec_pos,sPitheta_pos);
+ TH1F *h3_sPitheta_neg = (TH1F*)sPitheta_rec_neg->Clone("h3_sPitheta_neg"); h3_sPitheta_neg->Divide(sPitheta_rec_neg,sPitheta_neg);
+  
+ //---------------
+ TH1F *h3_P1phi_pos = (TH1F*)P1phi_rec_pos->Clone("h3_P1phi_pos"); h3_P1phi_pos->Divide(P1phi_rec_pos,P1phi_pos);
+ TH1F *h3_P1phi_neg = (TH1F*)P1phi_rec_neg->Clone("h3_P1phi_neg"); h3_P1phi_neg->Divide(P1phi_rec_neg,P1phi_neg);
+ 
+ TH1F *h3_P1eta_pos = (TH1F*)P1eta_rec_pos->Clone("h3_P1eta_pos"); h3_P1eta_pos->Divide(P1eta_rec_pos,P1eta_pos);
+ TH1F *h3_P1eta_neg = (TH1F*)P1eta_rec_neg->Clone("h3_P1eta_neg"); h3_P1eta_neg->Divide(P1eta_rec_neg,P1eta_neg);
+
+ TH1F *h3_P1pt_pos = (TH1F*)P1pt_rec_pos->Clone("h3_P1pt_pos"); h3_P1pt_pos->Divide(P1pt_rec_pos,P1pt_pos);
+  TH1F *h3_P1pt_neg = (TH1F*)P1pt_rec_neg->Clone("h3_P1pt_neg"); h3_P1pt_neg->Divide(P1pt_rec_neg,P1pt_neg);
+
+ TH1F *h3_P1theta_pos = (TH1F*)P1theta_rec_pos->Clone("h3_P1theta_pos"); h3_P1theta_pos->Divide(P1theta_rec_pos,P1theta_pos);
+ TH1F *h3_P1theta_neg = (TH1F*)P1theta_rec_neg->Clone("h3_P1theta_neg"); h3_P1theta_neg->Divide(P1theta_rec_neg,P1theta_neg);
+ //---------
+  TH1F *h3_P2phi_pos = (TH1F*)P2phi_rec_pos->Clone("h3_P2phi_pos"); h3_P2phi_pos->Divide(P2phi_rec_pos,P2phi_pos);
+ TH1F *h3_P2phi_neg = (TH1F*)P2phi_rec_neg->Clone("h3_P2phi_neg"); h3_P2phi_neg->Divide(P2phi_rec_neg,P2phi_neg);
+ 
+ TH1F *h3_P2eta_pos = (TH1F*)P2eta_rec_pos->Clone("h3_P2eta_pos"); h3_P2eta_pos->Divide(P2eta_rec_pos,P2eta_pos);
+ TH1F *h3_P2eta_neg = (TH1F*)P2eta_rec_neg->Clone("h3_P2eta_neg"); h3_P2eta_neg->Divide(P2eta_rec_neg,P2eta_neg);
+
+ TH1F *h3_P2pt_pos = (TH1F*)P2pt_rec_pos->Clone("h3_P2pt_pos"); h3_P2pt_pos->Divide(P2pt_rec_pos,P2pt_pos);
+  TH1F *h3_P2pt_neg = (TH1F*)P2pt_rec_neg->Clone("h3_P2pt_neg"); h3_P2pt_neg->Divide(P2pt_rec_neg,P2pt_neg);
+
+ TH1F *h3_P2theta_pos = (TH1F*)P2theta_rec_pos->Clone("h3_P2theta_pos"); h3_P2theta_pos->Divide(P2theta_rec_pos,P2theta_pos);
+ TH1F *h3_P2theta_neg = (TH1F*)P2theta_rec_neg->Clone("h3_P2theta_neg"); h3_P2theta_neg->Divide(P2theta_rec_neg,P2theta_neg);
+ 
+ //-----------------
+  TH1F *h3_D0phi_pos = (TH1F*)D0phi_rec_pos->Clone("h3_D0phi_pos"); h3_D0phi_pos->Divide(D0phi_rec_pos,D0phi_pos);
+ TH1F *h3_D0phi_neg = (TH1F*)D0phi_rec_neg->Clone("h3_D0phi_neg"); h3_D0phi_neg->Divide(D0phi_rec_neg,D0phi_neg);
+ 
+ TH1F *h3_D0eta_pos = (TH1F*)D0eta_rec_pos->Clone("h3_D0eta_pos"); h3_D0eta_pos->Divide(D0eta_rec_pos,D0eta_pos);
+ TH1F *h3_D0eta_neg = (TH1F*)D0eta_rec_neg->Clone("h3_D0eta_neg"); h3_D0eta_neg->Divide(D0eta_rec_neg,D0eta_neg);
+
+ TH1F *h3_D0pt_pos = (TH1F*)D0pt_rec_pos->Clone("h3_D0pt_pos"); h3_D0pt_pos->Divide(D0pt_rec_pos,D0pt_pos);
+  TH1F *h3_D0pt_neg = (TH1F*)D0pt_rec_neg->Clone("h3_D0pt_neg"); h3_D0pt_neg->Divide(D0pt_rec_neg,D0pt_neg);
+
+ TH1F *h3_D0theta_pos = (TH1F*)D0theta_rec_pos->Clone("h3_D0theta_pos"); h3_D0theta_pos->Divide(D0theta_rec_pos,D0theta_pos);
+ TH1F *h3_D0theta_neg = (TH1F*)D0theta_rec_neg->Clone("h3_D0theta_neg"); h3_D0theta_neg->Divide(D0theta_rec_neg,D0theta_neg);
  
  
- cout << P1_pos_rec<< " " << P1_pos<<endl;
+//--------
+ 
+   TH1F *h3_Dstphi_pos = (TH1F*)Dstphi_rec_pos->Clone("h3_Dstphi_pos"); h3_Dstphi_pos->Divide(Dstphi_rec_pos,Dstphi_pos);
+ TH1F *h3_Dstphi_neg = (TH1F*)Dstphi_rec_neg->Clone("h3_Dstphi_neg"); h3_Dstphi_neg->Divide(Dstphi_rec_neg,Dstphi_neg);
+ 
+ TH1F *h3_Dsteta_pos = (TH1F*)Dsteta_rec_pos->Clone("h3_Dsteta_pos"); h3_Dsteta_pos->Divide(Dsteta_rec_pos,Dsteta_pos);
+ TH1F *h3_Dsteta_neg = (TH1F*)Dsteta_rec_neg->Clone("h3_Dsteta_neg"); h3_Dsteta_neg->Divide(Dsteta_rec_neg,Dsteta_neg);
+
+ TH1F *h3_Dstpt_pos = (TH1F*)Dstpt_rec_pos->Clone("h3_Dstpt_pos"); h3_Dstpt_pos->Divide(Dstpt_rec_pos,Dstpt_pos);
+  TH1F *h3_Dstpt_neg = (TH1F*)Dstpt_rec_neg->Clone("h3_Dstpt_neg"); h3_Dstpt_neg->Divide(Dstpt_rec_neg,Dstpt_neg);
+
+ TH1F *h3_Dsttheta_pos = (TH1F*)Dsttheta_rec_pos->Clone("h3_Dsttheta_pos"); h3_Dsttheta_pos->Divide(Dsttheta_rec_pos,Dsttheta_pos);
+ TH1F *h3_Dsttheta_neg = (TH1F*)Dsttheta_rec_neg->Clone("h3_Dsttheta_neg"); h3_Dsttheta_neg->Divide(Dsttheta_rec_neg,Dsttheta_neg);
+ 
+ 
+ TCanvas * c1a= new TCanvas("c1a","c1a",400,10,600,400);
+c1a->Divide(2,2);
+c1a->cd(1);
+h3_sPiphi_pos->Draw("AE");
+c1a->cd(2);
+h3_sPieta_pos->Draw("AE"); 
+c1a->cd(3);
+h3_sPipt_pos->Draw("AE"); 
+c1a->cd(4);
+h3_sPitheta_pos->Draw("AE"); 
+ 
+TCanvas * c2a= new TCanvas("c2a","c2a",400,10,600,400);
+c2a->Divide(2,2);
+c2a->cd(1);
+h3_sPiphi_neg->Draw("AE");
+c2a->cd(2);
+h3_sPieta_neg->Draw("AE"); 
+c2a->cd(3);
+h3_sPipt_neg->Draw("AE"); 
+c2a->cd(4);
+h3_sPitheta_neg->Draw("AE"); 
+ 
+ //--------
+ 
+  TCanvas * c1b= new TCanvas("c1b","c1b",400,10,600,400);
+c1b->Divide(2,2);
+c1b->cd(1);
+h3_P1phi_pos->Draw("AE");
+c1b->cd(2);
+h3_P1eta_pos->Draw("AE"); 
+c1b->cd(3);
+h3_P1pt_pos->Draw("AE"); 
+c1b->cd(4);
+h3_P1theta_pos->Draw("AE"); 
+ 
+TCanvas * c2b= new TCanvas("c2b","c2b",400,10,600,400);
+c2b->Divide(2,2);
+c2b->cd(1);
+h3_P1phi_neg->Draw("AE");
+c2b->cd(2);
+h3_P1eta_neg->Draw("AE"); 
+c2b->cd(3);
+h3_P1pt_neg->Draw("AE"); 
+c2b->cd(4);
+h3_P1theta_neg->Draw("AE"); 
+ 
+ //-------
+   TCanvas * c1c= new TCanvas("c1c","c1c",400,10,600,400);
+c1c->Divide(2,2);
+c1c->cd(1);
+h3_P2phi_pos->Draw("AE");
+c1c->cd(2);
+h3_P2eta_pos->Draw("AE"); 
+c1c->cd(3);
+h3_P2pt_pos->Draw("AE"); 
+c1c->cd(4);
+h3_P2theta_pos->Draw("AE"); 
+ 
+TCanvas * c2c= new TCanvas("c2c","c2c",400,10,600,400);
+c2c->Divide(2,2);
+c2c->cd(1);
+h3_P2phi_neg->Draw("AE");
+c2c->cd(2);
+h3_P2eta_neg->Draw("AE"); 
+c2c->cd(3);
+h3_P2pt_neg->Draw("AE"); 
+c2c->cd(4);
+h3_P2theta_neg->Draw("AE"); 
+ 
+ //-----
+ 
+TCanvas * c1d= new TCanvas("c1d","c1d",400,10,600,400);
+c1d->Divide(2,2);
+c1d->cd(1);
+h3_D0phi_pos->Draw("AE");
+c1d->cd(2);
+h3_D0eta_pos->Draw("AE"); 
+c1d->cd(3);
+h3_D0pt_pos->Draw("AE"); 
+c1d->cd(4);
+h3_D0theta_pos->Draw("AE"); 
+ 
+TCanvas * c2d= new TCanvas("c2d","c2d",400,10,600,400);
+c2d->Divide(2,2);
+c2d->cd(1);
+h3_D0phi_neg->Draw("AE");
+c2d->cd(2);
+h3_D0eta_neg->Draw("AE"); 
+c2d->cd(3);
+h3_D0pt_neg->Draw("AE"); 
+c2d->cd(4);
+h3_D0theta_neg->Draw("AE"); 
+ 
+ //------
+    TCanvas * c1e= new TCanvas("c1e","c1e",400,10,600,400);
+c1e->Divide(2,2);
+c1e->cd(1);
+h3_Dstphi_pos->Draw("AE");
+c1e->cd(2);
+h3_Dsteta_pos->Draw("AE"); 
+c1e->cd(3);
+h3_Dstpt_pos->Draw("AE"); 
+c1e->cd(4);
+h3_Dsttheta_pos->Draw("AE"); 
+ 
+TCanvas * c2e= new TCanvas("c2e","c2e",400,10,600,400);
+c2e->Divide(2,2);
+c2e->cd(1);
+h3_Dstphi_neg->Draw("AE");
+c2e->cd(2);
+h3_Dsteta_neg->Draw("AE"); 
+c2e->cd(3);
+h3_Dstpt_neg->Draw("AE"); 
+c2e->cd(4);
+h3_Dsttheta_neg->Draw("AE"); 
  
  
  
  
  
+ 
+ 
+
  
  
  
 }
+
